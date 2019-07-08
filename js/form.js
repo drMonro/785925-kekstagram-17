27a267f76aac1ef5fd91015450ee1a0f671be7c5
drmonro <rapman477@ya.ru>
2019-07-09 03:43:55 +0700

    Валидация хэш тэгов

--- js/form.js
+++ js/form.js
@@ -7,8 +7,54 @@
   var elementPopupClose = document.getElementById('upload-cancel');
   var uploadWindow = document.querySelector('.img-upload');
   var commentInputFocusStatus = false;
+  var tagInputFocusStatus = false;
   var commentInput = uploadWindow.querySelector('.text__description');
   var uploadFileArea = document.getElementById('upload-file');
+  var hashTagsInput = imageEditorForm.querySelector('.text__hashtags');
+
+
+  function onHashTagsInputValidation() {
+    var hashTags = hashTagsInput.value
+      .split(' ')
+      .map(function (hashTag) {
+        return hashTag.toLowerCase();
+      });
+
+    var message = '';
+
+    if (hashTags.length > 5) {
+      message = 'Нельзя указать больше пяти хэш-тегов';
+    } else {
+      for (var i = 0; i < hashTags.length; i++) {
+        message = validationHashTag(hashTags, i);
+        if (message) {
+          break;
+        }
+      }
+    }
+
+    hashTagsInput.setCustomValidity(message);
+  }
+
+  function validationHashTag(hashTags, i) {
+    var message = '';
+    if (hashTags[i].charAt(0) !== '#') {
+      message = 'Хеш-теги должны начинаться с "#"';
+
+    } else if (hashTags[i].length === 1) {
+      message = 'Хеш-теги должны состоять хотя бы из одного символа';
+
+    } else if (hashTags[i].indexOf('#', 1) > 0) {
+      message = 'Хеш-теги должны разделяться пробелами';
+
+    } else if (hashTags.indexOf(hashTags[i], i + 1) > 0) {
+      message = 'Один и тот же хэш-тег не может быть использован дважды';
+
+    } else if (hashTags[i].length > 20) {
+      message = 'Максимальная длина одного хэш-тега 20 символов';
+    }
+    return message;
+  }
 
 
   var openImageEditorPopup = function (imageEditor, closingKeyCode) {
@@ -19,7 +65,7 @@
   };
 
   var closeOnPressKey = function (evt, popup, closingKeyCode) {
-    if (evt.keyCode === closingKeyCode && !commentInputFocusStatus) {
+    if (evt.keyCode === closingKeyCode && !commentInputFocusStatus && !tagInputFocusStatus) {
       closeImageEditorPopup(popup, closingKeyCode);
     }
   };
@@ -54,6 +100,16 @@
   commentInput.addEventListener('focusout', function () {
     commentInputFocusStatus = false;
   });
+
+  hashTagsInput.addEventListener('focus', function () {
+    commentInputFocusStatus = true;
+  });
+
+  hashTagsInput.addEventListener('focusout', function () {
+    commentInputFocusStatus = false;
+  });
+
+  hashTagsInput.addEventListener('input', onHashTagsInputValidation);
 
 
   window.form = {
